a774f7f997c0f657d5515a8684a26b4e7179a42d
Tahirnawaz <tahirnawazwebpro@gmail.com>
2025-09-05 00:36:31 +0500

    run scaffolding

--- SmartPark/SmartPark/Models/Feedback.cs
+++ SmartPark/SmartPark/Models/Feedback.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace SmartPark.Models;
 
+[Table("Feedback")]
 public partial class Feedback
 {
+    [Key]
     public Guid Id { get; set; }
 
+    [StringLength(255)]
+    [Unicode(false)]
     public string? Description { get; set; }
 
     public int? Rating { get; set; }
 
+    [Column(TypeName = "datetime")]
     public DateTime? TimeStamp { get; set; }
 
     public Guid? UserId { get; set; }
 
     public Guid? LocationId { get; set; }
 
+    [ForeignKey("LocationId")]
+    [InverseProperty("Feedbacks")]
     public virtual ParkingLocation? Location { get; set; }
 
+    [ForeignKey("UserId")]
+    [InverseProperty("Feedbacks")]
     public virtual User? User { get; set; }
 }
